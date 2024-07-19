081b07582d77310c935343d81dbf43bf1eeb0d1b
MyProgramings <reader1999a@gmail.com>
2024-07-19 15:38:05 +0300

    done hide fortify

--- resources/views/parts/structure.blade.php
+++ resources/views/parts/structure.blade.php
@@ -6,7 +6,9 @@
         <div class="card-news">
             <div class="row">
                 <div class="col-md-12">
-                    <img src="{{ asset('images/structure.png') }}" alt="...">
+                    <a href="{{ asset('images/structure.png') }}" target="_blank">
+                        <img src="{{ asset('images/structure.png') }}" alt="...">
+                    </a>
                 </div>
             </div>
         </div>
